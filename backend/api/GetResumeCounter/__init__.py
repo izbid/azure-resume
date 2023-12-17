19d330f401c38b8cb8879d41162e53d5b4c27a5b
tizbid <tizbid@gmail.com>
2023-12-17 05:14:59 +0100

    First commit; Azure function tested & working

---
+++ backend/api/GetResumeCounter/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+
+# HTTP Request
+def main(req: func.HttpRequest, inputDocument: func.DocumentList,
+         outputDocument: func.Out[func.Document]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    counter = getNewCounterValue(inputDocument[0]['count'])
+    inputDocument[0]['count'] = counter
+    outputDocument.set(func.Document.from_json(inputDocument[0].to_json()))
+    if counter:
+        return func.HttpResponse(f"This page have been viewed: {counter} times", status_code=200)
+    else:
+        return func.HttpResponse(
+            "Error",
+            status_code=500)
+
+
+def getNewCounterValue(value: int):
+    return value + 1
